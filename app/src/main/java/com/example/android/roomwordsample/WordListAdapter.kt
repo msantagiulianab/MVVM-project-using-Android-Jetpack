3fcf1b65e67a0a16919bb504a8728e5eb86225be
Michele Santagiuliana Busellato <michelesantagiuliana@hotmail.it>
2021-02-03 14:38:34 +0000

    Now using View Binding

---
+++ app/src/main/java/com/example/android/roomwordsample/WordListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.android.roomwordsample
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.roomwordsample.databinding.RecyclerviewItemBinding
+
+class WordListAdapter : ListAdapter<Word, WordListAdapter.WordViewHolder>(WordsComparator()) {
+
+    class WordViewHolder(val binding: RecyclerviewItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+
+        return WordViewHolder(
+            RecyclerviewItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+
+        fun bind(text: String?) {
+            holder.binding.textView.text = text
+        }
+
+        val current = getItem(position)
+        bind(current.word)
+    }
+
+
+    class WordsComparator : DiffUtil.ItemCallback<Word>() {
+        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem.word == newItem.word
+        }
+    }
+}
